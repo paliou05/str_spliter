6bfa491c4213e77edb468393dee45b30945093ac
Kostas Paliouras <paliou05@gmail.com>
2016-01-12 10:57:09 +0200

    check if its number

--- str_spliter.py
+++ str_spliter.py
@@ -2,7 +2,7 @@ import operator
 import random
 
 
-string = raw_input("Give your math type")
+string = raw_input("Give your math type\n>")
 index = 0
 op_ind = 0
 ops = {'+':operator.add,
@@ -29,15 +29,18 @@ if string.startswith('(') == True and string.endswith(')') == True:
         split_str = string.split(string[opidx])
         num1 = split_str[0].split('(')
         num2 = split_str[1].split(')')
-        a = int(num1[1])
-        b = int(num2[0])
+        try:
+                a = int(num1[1])
+                b = int(num2[0])
+        except ValueError:
+                print "\nThat's not a number, please enter only numbers\n"
         if b == 0 and op == list(ops.keys())[3]:
-                raise Exception("Division by 0 can't happen")
+                raise Exception("\nDivision by 0 can't happen\n")
         else:
                 answer = int(ops.get(op)(a,b))
                 print answer
 else:
-        print ("Forgot to close parenthesys")
+        print ("\nForgot to close parenthesys\n")
 
         
         
